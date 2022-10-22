489f616cbeacaa101bfad73ab2a78e0dbea00ec2
nirajksh <8809nirajsingh@gmail.com>
2022-10-22 17:25:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Resturant from "./component/basics/Resturant";
+//import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+//import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return<Resturant/>
+};
+
+export default App;
